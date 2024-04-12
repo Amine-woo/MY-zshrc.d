69df6eedf0f8beb647b3cd0b72196510df536af1
TheSola10 <me@thesola.io>
2024-04-12 19:20:16 +0200

    added bat syntax

--- flake.nix
+++ flake.nix
@@ -6,7 +6,10 @@
   flake-utils.lib.eachDefaultSystem
     (system:
     let
-      pkgs = import nixpkgs { inherit system; };
+      pkgs = import nixpkgs
+        { inherit system;
+          overlays = [];
+        };
     in
     { legacyPackages = pkgs;
       packages.default = self.packages.${system}.zsh;
